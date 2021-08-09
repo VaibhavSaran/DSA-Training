ab2850985688600d12af5979d6a7a287b8852cee
Vaibhav Saran <vaibhavsaran8@gmail.com>
2021-08-09 11:44:21 +0530

    Assignment 7 completed

--- Assignment 7/q2.cpp
+++ Assignment 7/q2.cpp
@@ -10,3 +10,36 @@ Explanation:The array elements satisfying the condition are:
 5 = 1 + 4 or 4 + 1
 Input: a[] = {1, 1, 1, 1, 1}
 Output: 0*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a[100],n,count[101],ans=0;
+	cout<<"Enter the number of elements in array: ";
+	cin>>n;
+	cout<<"Enter the array:\n";
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	memset(count,0,101); // to initialize each element of array with 0
+	// Updating frequency of each element of array
+	for(int i=0;i<n;i++)
+		++count[a[i]];
+	// finding the sum of all sub arrays (consecutive)
+	for(int l=0;l<n;++l)
+	{
+		int sum=0;
+		for(int r=l;r<n;r++)
+		{
+			sum+=a[r];
+			if(l==r)
+				continue;
+			if(sum<=n)
+			{
+				ans+=count[sum];
+				count[sum]=0;
+			}
+		}
+	}
+	cout<<"The number of elements are: "<<ans;
+	return 0;
+}
